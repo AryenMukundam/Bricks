b8ca0919ebec6e6a4964f55aa1b56bb239663c0c
AryenMukundam <aryenmukundam@gmail.com>
2025-10-22 21:12:52 +0530

    New commit

--- BRICKS_DASHBOARD/Backend/app.js
+++ BRICKS_DASHBOARD/Backend/app.js
@@ -10,7 +10,11 @@ import cookieParser from 'cookie-parser';
 
 connectToDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true, 
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
